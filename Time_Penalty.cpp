a1afde68efe3b9787d04105416d40c5006b9a06a
codewithaman07 <amansingh2009000@gmail.com>
2025-02-14 19:31:44 +0530

    Starters 173

---
+++ Time_Penalty.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define Aman() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+const int M = 1e9+7;
+const int N = 1e6 + 1;
+
+void solve() {
+    int x,y; cin>>x>>y;
+    cout<<(y*10)+x<<endl;
+}
+
+signed main() {
+    cout << fixed << setprecision(20);
+    Aman();
+
+    int tc = 1;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
